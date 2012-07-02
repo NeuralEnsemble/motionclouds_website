9a1a65f41f0d35135fa28483c18a8e6c8e63d297
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2012-07-02 15:21:08 +0200

    Adding an install script for the dependencies on debian systems (tested on a neurodebian VM) / fixed installation on debian to allow export to HDF files

---
+++ install_dependencies_debian.sh
@@ -0,0 +1,20 @@
+# installing dependencies on Debian for MotionClouds
+# --------------------------------------------------
+#
+# uncomment to fit your installation preference
+#
+
+#
+# minimal install 
+#
+# sudo aptitude install python-numpy python-scipy 
+
+#
+# minimal install with visualization
+#
+# sudo aptitude install python-numpy python-scipy mayavi2 python-matplotlib ffmpeg
+
+#
+# full install with python editor and libraries for various export types
+#
+sudo aptitude install python-numpy python-scipy mayavi2 python-matplotlib ffmpeg spyder liblzo2-2 python-tables imagemagick
