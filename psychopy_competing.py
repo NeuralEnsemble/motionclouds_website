b8b918e5dc88c5f91c1ffa2bfc0f7c8ae671c159
Laurent Perrinet <orez.org@gmail.com>
2012-12-14 12:26:07 +0100

    Improved psychopy example
    Feedback from
    https://groups.google.com/d/topic/psychopy-users/ubsGZbopkB0/discussion
    - moving from PatchStim (deprtecated) to GratingStim
    - generating just one object, then updating the texture
    - added some decorators (minimal fixation point and question mark)

--- psychopy_competing.py
+++ psychopy_competing.py
@@ -12,8 +12,8 @@ Using psychopy to perform an experiment on competing clouds
 w, h = 1920, 1200
 
 print('launching experiment')
-from psychopy import *
-import time
+from psychopy import visual, core, event, logging, gui, misc
+logging.console.setLevel(logging.DEBUG)
 
 try:
     #try to load previous info
@@ -40,6 +40,8 @@ if dlg.OK:
 else:
     print('Could not load gui... running with defaut parameters')
     #core.quit() #user cancelled. quit
+
+import time
 info['timeStr'] = time.strftime("%b_%d_%H%M", time.localtime())
 
 
@@ -56,6 +58,18 @@ down = 2*mc.rectif(mc.random_cloud(color * mc.envelope_gabor(fx, fy, ft, V_X=-.5
 print('go!      ')
 win = visual.Window([info['screen_width'], info['screen_height']], fullscr=True)
 
+stim = visual.GratingStim(win, 
+        size=(info['screen_height'], info['screen_height']), units='pix',
+        interpolate=False,
+        autoLog=False)#this stim changes too much for autologging to be useful
+
+wait_for_response = visual.TextStim(win, 
+                        text = u"?", units='norm', height=0.15, color='DarkSlateBlue',
+                        pos=[0., -0.], alignHoriz='center',alignVert='center' ) 
+wait_for_next = visual.TextStim(win, 
+                        text = u"x", units='norm', height=0.15, color='BlanchedAlmond',
+                        pos=[0., -0.], alignHoriz='center',alignVert='center' ) 
+                        
 def getResponse():
     event.clearEvents()#clear the event buffer to start with
     resp = None#initially
@@ -63,7 +77,7 @@ def getResponse():
         for key in event.getKeys():
             #quit
             if key in ['escape', 'q']:
-                    myWin.close()
+                    win.close()
                     #myWin.bits.reset()
                     core.quit()
                     return None
@@ -80,25 +94,22 @@ def presentStimulus(C_A, C_B):
     phase_up = numpy.floor(numpy.random.rand() *(info['N_frame_total']-info['N_frame']))
     phase_down = numpy.floor(numpy.random.rand() *(info['N_frame_total']-info['N_frame']))
     for i_frame in range(info['N_frame']): # length of the stimulus
-        stim = visual.PatchStim(win,
-            tex=C_A * up[:, :, i_frame+phase_up]+C_B * down[:, :, i_frame+phase_down],
-            size=(info['screen_height'], info['screen_height']), units='pix',
-            interpolate=False)
+        stim.setTex(C_A * up[:, :, i_frame+phase_up]+C_B * down[:, :, i_frame+phase_down])
         stim.draw()
-        stim.clearTextures()
         win.flip()
-    stim.clearTextures()
-    win.update()
 
 results = numpy.zeros((2, info['nTrials']))
 for i_trial in range(info['nTrials']):
+    wait_for_next.draw()
+    win.flip()
+    core.wait(0.5)
     C_A = numpy.random.rand() # a random number between 0 and 1
     presentStimulus(C_A, 1. - C_A)
+    wait_for_response.draw()
+    win.flip()
     ans = getResponse()
     results[0, i_trial] = ans
     results[1, i_trial] = C_A
-    core.wait(0.5)
-
 
 win.update()
 core.wait(0.5)
@@ -110,6 +121,7 @@ fileName = 'data/' + info['observer'] + '_' + info['timeStr']
 numpy.save(fileName,results)
 
 print('analyzing results')
+# TODO: loop over all data + make a fit for each
 import pylab
 pylab.scatter(results[1, :], results[0, :])
 pylab.axis([0., 1., -1.1, 1.1])
--- psychopy_simple.py
+++ psychopy_simple.py
@@ -16,15 +16,17 @@ color = mc.envelope_color(fx, fy, ft)
 env = color *(mc.envelope_gabor(fx, fy, ft, V_X=1.) + mc.envelope_gabor(fx, fy, ft, V_X=-1.))
 z = 2*mc.rectif(mc.random_cloud(env), contrast=.5) -1.
 
-# TODO : make a GUI with two threads: one for psychopy, one for the generation / see also experiment_smooth.py for a smooth transition of parameters
-from psychopy import *
-import time
+from psychopy import visual, core, event, logging
+logging.console.setLevel(logging.DEBUG)
+
 win = visual.Window([w, h], fullscr=True)
+stim = visual.GratingStim(win, #tex=noiseTexture, 
+        size=(600, 600), units='pix',
+        interpolate=False,
+        autoLog=False)#this stim changes too much for autologging to be useful
+
 for i_frame in range(mc.N_frame):
     #creating a new stimulus every time
-    stim = visual.PatchStim(win, tex=z[:, :, i_frame],
-        size=(600, 600), units='pix',
-        interpolate=False)
+    stim.setTex(z[:, :, i_frame])
     stim.draw()
-    stim.clearTextures()
     win.flip()
