c9f3f05396f4711464cd04eb544c7b269e630162
Laurent Perrinet <laurent.perrinet@gmail.com>
2013-02-12 14:45:41 +0100

    updating TODO

--- TODO.md
+++ TODO.md
@@ -4,7 +4,7 @@ List of things to improve in the code
 (by order of priority)
 
 * make a class out of the `MotionClouds.py` library
-* use a trick (forcing the phase) to generate concentric waves -> extend to experimentally show Kelvin's wake waves
+* experiment_concentric.py : use a trick (forcing the phase) to generate concentric waves -> extend to experimentally show Kelvin's wake waves
 * make a warning when the choice of parameter is not completley sane (for instance f_0 or B_sf greater than the Nyquist frequency of 0.5)
 * use a Laplace (in itme) - Fourier (in space) transform to generate Motion Clouds as a stream of frames instead of the whole memory (would be good for memory concerns)
-* use glumpy for displaying the movie online : fullscreen
+* DONE: use glumpy for displaying the movie online : fullscreen
--- experiment_concentric.py
+++ experiment_concentric.py
@@ -2,6 +2,10 @@
 """
 Averaging over multiple clouds 
 
+TODO: force the phase by setting the luminance at some point and averaging voer multiple instances
+
+rm results/concentric.mp4; python experiment_concentric.py; open results/concentric.mp4 
+
 (c) Laurent Perrinet - INT/CNRS
 
 """
@@ -11,7 +15,7 @@ import numpy as np
 
 name = 'concentric'
 play = False #True
-#play = True
+play = True
 
 #initialize
 fx, fy, ft = mc.get_grids(mc.N_X, mc.N_Y, mc.N_frame)
@@ -27,7 +31,7 @@ N = 20
 
 if mc.anim_exist(name_):
     for i_N in xrange(N):
-        im_ = mc.random_cloud(color * mc.envelope_gabor(fx, fy, ft), seed=seed+i_N)
+        im_ = mc.random_cloud(color * mc.envelope_gabor(fx, fy, ft, V_X=0., V_Y=0.), seed=seed+i_N)
         if i_N == 0:
             phase = 0.5 + 0. * im_[0, 0, :]#mc.N_X/2, mc.N_Y/2, :]
         #im += im_ - im_[mc.N_X/2, mc.N_Y/2, :] + phase
