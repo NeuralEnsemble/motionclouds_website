481df0395d284ef9d627e5daf06d68469878c176
Laurent Perrinet <laurent.perrinet@gmail.com>
2013-02-12 14:34:40 +0100

    trying to generate motion clouds where we force the phase at some point

---
+++ experiment_concentric.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""
+Averaging over multiple clouds 
+
+(c) Laurent Perrinet - INT/CNRS
+
+"""
+
+import MotionClouds as mc
+import numpy as np
+
+name = 'concentric'
+play = False #True
+#play = True
+
+#initialize
+fx, fy, ft = mc.get_grids(mc.N_X, mc.N_Y, mc.N_frame)
+color = mc.envelope_color(fx, fy, ft)
+
+name_ = mc.figpath + name
+
+seed = 123456
+im = np.zeros((mc.N_X, mc.N_Y, mc.N_frame))
+name_ = mc.figpath + name
+
+N = 20
+
+if mc.anim_exist(name_):
+    for i_N in xrange(N):
+        im_ = mc.random_cloud(color * mc.envelope_gabor(fx, fy, ft), seed=seed+i_N)
+        if i_N == 0:
+            phase = 0. * im_[mc.N_X/2, mc.N_Y/2, :]
+        im += im_ - im_[mc.N_X/2, mc.N_Y/2, :] + phase
+
+    if play:
+        mc.play(mc.rectif(im))
+    else:
+        mc.anim_save(mc.rectif(im), name_)
+    #    mplayer figures/concentric.mpg -fs -loop 0
