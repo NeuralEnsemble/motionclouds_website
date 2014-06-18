e9eb152ee4f0b8b2b11f42b37b5a498bbae7bccf
Laurent Perrinet <laurent.perrinet@gmail.com>
2014-06-18 17:16:12 +0200

    Update

--- test_grating.py
+++ test_grating.py
@@ -17,32 +17,31 @@ name = 'grating'
 fx, fy, ft = mc.get_grids(mc.N_X, mc.N_Y, mc.N_frame)
 color = mc.envelope_color(fx, fy, ft)
 
-name_ = mc.figpath + name
 z = color * mc.envelope_gabor(fx, fy, ft)
-mc.figures(z, name_)
+mc.figures(z, name)
 
 # explore parameters
-for sparseness in numpy.linspace(.9, 4., 8):
-    name_ = mc.figpath + name + '-sparseness-' + str(sparseness).replace('.', '_')
+for sparseness in np.linspace(.9, 4., 8):
+    name_ = name + '-sparseness-' + str(sparseness).replace('.', '_')
     mc.figures_MC(fx, fy, ft, name_, do_amp=True, B_V=0.01, sparseness=sparseness)
 
 for sigma_div in [1, 2, 3, 5, 8, 13 ]:
-    name_ = mc.figpath + name + '-largeband-B_theta-pi-over-' + str(sigma_div).replace('.', '_')
-    z = color * mc.envelope_gabor(fx, fy, ft, B_theta=numpy.pi/sigma_div)
+    name_ = name + '-largeband-B_theta-pi-over-' + str(sigma_div).replace('.', '_')
+    z = color * mc.envelope_gabor(fx, fy, ft, B_theta=np.pi/sigma_div)
     mc.figures(z, name_)
 
 for div in [1, 2, 4, 3, 5, 8, 13, 20, 30]:
-    name_ = mc.figpath + name + '-theta-pi-over-' + str(div).replace('.', '_')
-    z = color * mc.envelope_gabor(fx, fy, ft, theta=numpy.pi/div)
+    name_ = name + '-theta-pi-over-' + str(div).replace('.', '_')
+    z = color * mc.envelope_gabor(fx, fy, ft, theta=np.pi/div)
     mc.figures(z, name_)
 
 V_X = 1.0
 for sigma_div in [1, 2, 3, 5, 8, 13 ]:
-    name_ = mc.figpath + name + '-B_theta-pi-over-' + str(sigma_div).replace('.', '_') + '-V_X-' + str(V_X).replace('.', '_')
-    z = color * mc.envelope_gabor(fx, fy, ft, V_X=V_X, B_theta=numpy.pi/sigma_div)
+    name_ = name + '-B_theta-pi-over-' + str(sigma_div).replace('.', '_') + '-V_X-' + str(V_X).replace('.', '_')
+    z = color * mc.envelope_gabor(fx, fy, ft, V_X=V_X, B_theta=np.pi/sigma_div)
     mc.figures(z, name_)
 
 for B_sf in [0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.8]:
-    name_ = mc.figpath + name + '-B_sf-' + str(B_sf).replace('.', '_')
+    name_ = name + '-B_sf-' + str(B_sf).replace('.', '_')
     z = color * mc.envelope_gabor(fx, fy, ft, B_sf=B_sf)
     mc.figures(z, name_)
